bb0ffdc009d95ef8ef555a2c8de4918e2ad3d6f8
Marc-Alban <millet.marcalban@gmail.com>
2019-09-23 15:44:33 +0200

    Ajout du commentManagers

---
+++ src/model/backend/CommentManager.php
@@ -0,0 +1,65 @@
+<?php
+declare (strict_types = 1);
+namespace Blog\Model\Backend;
+
+use Blog\Model\Database;
+use \PDO;
+
+class CommentManager
+{
+/**
+ * Affiche les commentaires écrit dans le front sur le dashboard
+ *
+ * @return array
+ */
+    public function getComments(): array
+    {
+
+        $query = Database::getDb()->query("
+    SELECT  comments.id,
+            comments.name,
+            comments.date_comment,
+            comments.post_id,
+            comments.comment,
+            posts.title
+    FROM    comments
+    JOIN    posts
+    ON      comments.post_id = posts.id
+    WHERE   comments.seen = '1'
+    ORDER BY comments.date_comment ASC
+    ");
+        $results = $query->fetchAll(PDO::FETCH_OBJ);
+        return $results;
+    }
+
+/**
+ * Permet de supprimer un commentaire en bdd
+ *
+ * @param integer $id
+ * @return void
+ */
+    public function deleteComments(int $id): void
+    {
+        $query = Database::getDb()->prepare("DELETE FROM comments WHERE id = :id");
+        $query->execute(["id" => $id]);
+    }
+
+/**
+ * Permet de valider le commentaire et de le remettre à la valeur 0 dans la table seen
+ *
+ * @param integer $id
+ * @return void
+ */
+    public function validateComments(int $id): void
+    {
+        $query = Database::getDb()->prepare("UPDATE comments SET seen = '0' WHERE id = :id");
+        $query->execute(["id" => $id]);
+    }
+
+    public function nbComments()
+    {
+        $query = Database::getDb()->query("SELECT DISTINCT post_id, COUNT(seen) FROM comments WHERE seen = '1' GROUP BY post_id");
+        $result = $query->fetch(PDO::FETCH_OBJ);
+        return $result;
+    }
+}
---
+++ templates/backend/adminCommentsView.php
@@ -0,0 +1,78 @@
+<div class="container">
+    <div class="row">
+        <h1>Commentaires signalés:</h1>
+        <!-- Table - Listes commentaires -->
+        <table class="table">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">Nom</th>
+                    <th scope="col">Commentaires</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php if (!empty($datas['comments'])): ?>
+                <?php foreach ($datas['comments'] as $comment): ?>
+                <tr id="commentaire_<?=$comment->id?>">
+                    <th scope="row"><?=$comment->title?></th>
+                    <td><?=substr($comment->comment, 0, 100)?>...</td>
+                    <td>
+                        <a href="index.php?page=admin&id=<?=$comment->id?>&action=valideComment" id="<?=$comment->id?>"
+                            class="see_comment"><button class="btn btn-primary btn-circle btn-lg see_comment"><i
+                                    class="fas fa-check-circle"></i></button></a>
+
+                        <a href="index.php?page=admin&id=<?=$comment->id?>&action=removeComment" id="<?=$comment->id?>"
+                            class="delete_comment"><button class="btn btn-warning btn-circle btn-lg delete_comment"><i
+                                    class="fas fa-trash-alt"></i></button></a>
+
+                        <!-- modal -->
+                        <!-- Button trigger modal -->
+                        <div class="btn-group">
+
+                            <button type="button" class="btn btn-success" data-toggle="modal"
+                                data-target="#commentaires_<?=$comment->id?>">
+                                <i class="fas fa-window-restore"></i>
+                            </button>
+                        </div>
+
+                        <!-- Modal -->
+                        <div class="modal fade-scale" id="commentaires_<?=$comment->id?>">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <span data-dismiss="modal" aria-hidden="true">×</span>
+                                        <h4 class="modal-title"><?=$comment->title?></h4>
+                                    </div>
+                                    <div class="modal-body">
+                                        <p><strong>Commentaire publié par <?=$comment->name?>,
+                                                le
+                                                <?php echo date("d/m/Y à H:i:s", strtotime($comment->date_comment)) ?></strong>
+                                        </p>
+                                        <p><?=nl2br($comment->comment)?></p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <a href="index.php?page=admin&id=<?=$comment->id?>&action=valideComment"
+                                            id="<?=$comment->id?>" class="see_comment"><button
+                                                class="btn btn-primary btn-circle btn-lg see_comment"><i
+                                                    class="fas fa-check-circle"></i></button></a>
+
+                                        <a href="index.php?page=admin&id=<?=$comment->id?>&action=removeComment"
+                                            id="<?=$comment->id?>" class="delete_comment"><button
+                                                class="btn btn-warning btn-circle btn-lg delete_comment"><i
+                                                    class="fas fa-trash-alt"></i></button></a>
+                                    </div>
+
+                                </div><!-- /.modal-content -->
+                            </div><!-- /.modal-dialog -->
+                        </div><!-- /.modal -->
+                        <!-- end modal -->
+                    </td>
+                </tr>
+                <?php endforeach?>
+                <?php else:echo '<tr><td><center>Aucun commentaires dans le tableau</center></td></tr>';?>
+                <?php endif?>
+            </tbody>
+        </table>
+        <!-- fin table -->
+    </div>
+</div>
