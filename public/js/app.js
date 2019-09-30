0d33bc120f1e843e9c5caac761e58f3f3746be07
Marc-Alban <millet.marcalban@gmail.com>
2019-09-30 10:15:20 +0200

    Problème de commentaire résolue

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,22 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
   var name = document.getElementById("name");
-  name.addEventListener("input", function(e) {
-    var name = e.target.value; // Valeur saisie dans le champ name
-    var longueurName = "faible";
-    var couleurMsg = "red"; // Longueur faible => couleur rouge
-    if (name.length >= 8) {
-      longueurName = "suffisante";
-      couleurMsg = "green"; // Longueur suffisante => couleur verte
-    } else if (name.length >= 4) {
-      longueurName = "moyenne";
-      couleurMsg = "orange"; // Longueur moyenne => couleur orange
-    }
-    var aideNameElt = document.getElementById("aideName");
-    aideNameElt.textContent = "Longueur : " + longueurName; // Texte de l'aide
-    aideNameElt.style.color = couleurMsg; // Couleur du texte de l'aide
-  });
+  if (name) {
+    name.addEventListener("input", function (e) {
+      var name = e.target.value; // Valeur saisie dans le champ name
+      var longueurName = "faible";
+      var couleurMsg = "red"; // Longueur faible => couleur rouge
+      if (name.length >= 8) {
+        longueurName = "suffisante";
+        couleurMsg = "green"; // Longueur suffisante => couleur verte
+      } else if (name.length >= 4) {
+        longueurName = "moyenne";
+        couleurMsg = "orange"; // Longueur moyenne => couleur orange
+      }
+      var aideNameElt = document.getElementById("aideName");
+      aideNameElt.textContent = "Longueur : " + longueurName; // Texte de l'aide
+      aideNameElt.style.color = couleurMsg; // Couleur du texte de l'aide
+    });
+  }
 
 });
