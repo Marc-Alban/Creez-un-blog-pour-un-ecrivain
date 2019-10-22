3a9d5648c6867fcbb646ceed7f60068c4f410785
Marc-Alban <millet.marcalban@gmail.com>
2019-10-22 20:25:50 +0200

    Modification src

--- src/model/Database.php
+++ src/model/Database.php
@@ -6,12 +6,9 @@ use \PDO;
 
 class Database
 {
-    const DSN = 'mysql:host=db5000190822.hosting-data.io;dbname=dbs185636';
-    const USER = 'dbu94874';
-    const PASSWORD = 'Admin123Test..';
-    // const DSN = 'mysql:host=localhost;dbname=blog';
-    // const USER = 'root';
-    // const PASSWORD = '';
+    const DSN = 'mysql:host=localhost;dbname=blog';
+    const USER = 'root';
+    const PASSWORD = '';
     private static $db = null;
 
     /**
